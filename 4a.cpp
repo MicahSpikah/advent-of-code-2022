0d5a20623a27d789a5ebe75c4481ee4020d01778
Micah TerHaar <micah.terhaar@gentex.com>
2022-12-04 08:16:01 -0500

    4a

---
+++ 4a.cpp
@@ -0,0 +1,45 @@
+#include "parse_input.h"
+
+struct parsed_sections
+{
+    int min0;
+    int max0;
+    int min1;
+    int max1;
+};
+
+std::vector< parsed_sections > parse_sections( std::vector< std::string > const& input )
+{
+    std::vector< parsed_sections > out;
+
+    for( auto const& str : input )
+    {
+        auto const elf{ []( auto const& raw_elf ) {
+            auto const dash_index{ raw_elf.find( '-' ) };
+            return std::make_pair< int, int >( std::stoi( raw_elf.substr( 0, dash_index ) ), std::stoi( raw_elf.substr( dash_index + 1 ) ) );
+        } };
+
+        auto const comma_index{ str.find( ',' ) };
+        auto const elf0{ elf( str.substr( 0, comma_index ) ) };
+        auto const elf1{ elf( str.substr( 1 + comma_index ) ) };
+        out.push_back( parsed_sections{ elf0.first, elf0.second, elf1.first, elf1.second } );
+    }
+    return out;
+}
+
+int main( int const argc, char* const argv[] )
+{
+    auto const raw_input{ parse_input( argc, argv ) };
+    auto const sections{ parse_sections( raw_input ) };
+
+    int sum{};
+    for( auto const& section : sections )
+    {
+        if( ( section.min0 <= section.min1 && section.max0 >= section.max1 ) || ( section.min1 <= section.min0 && section.max1 >= section.max0 ) )
+        {
+            ++sum;
+        }
+    }
+
+    std::cout << sum << '\n';
+}
---
+++ template.cpp
@@ -0,0 +1,6 @@
+#include "parse_input.h"
+
+int main( int const argc, char* const argv[] )
+{
+    auto const input{ parse_input( argc, argv ) };
+}

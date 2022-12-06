8abb23fd49841de9a1f9cda809fb385149bddef1
Micah TerHaar <micah.terhaar@gentex.com>
2022-12-06 13:26:55 -0500

    Day 6

---
+++ 6b.cpp
@@ -0,0 +1,21 @@
+#include "parse_input.h"
+#include <unordered_set>
+
+static int constexpr header_size{ 14 };
+
+int main( int const argc, char* const argv[] )
+{
+    auto const input{ parse_input( argc, argv ) };
+
+    std::string const& buffer{ input.front() };
+
+    for( int i{}; i + header_size - 1 < buffer.size(); ++i )
+    {
+        if( std::unordered_set< char >( buffer.cbegin() + i, buffer.cbegin() + i + header_size ).size() == header_size )
+        {
+            std::cout << ( i + header_size ) << '\n';
+            std::exit( 0 );
+        }
+    }
+    return 1;
+}

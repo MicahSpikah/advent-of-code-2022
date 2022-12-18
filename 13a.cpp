b885ac05fcaa66e7555b5d5f27865d06a4a0e9c0
Micah TerHaar <micah.terhaar@gentex.com>
2022-12-17 22:01:58 -0500

    Getting into the three-way-operator life. 13a passing

--- 13a.cpp
+++ 13a.cpp
@@ -16,18 +16,24 @@ struct indent_up
     }
 };
 
+std::ostream& dbg_indented()
+{
+    return std::cout << std::string( indent, ' ' );
+}
+
 struct thing;
-std::ostream& operator<<(std::ostream& os, thing const& t);
+std::ostream& operator<<( std::ostream& os, thing const& t );
 
 struct thing
 {
     thing( std::string const& text )
     {
         bool constructor_output{};
-        indent_up name_this_to_keep_alive_for_raii;
 
         if( constructor_output )
-            std::cout << std::string( indent - 1, ' ' ) << "Parsing: " << text << '\n';
+            dbg_indented() << "Parsing: "s << text << '\n';
+
+        //indent_up name_this_to_keep_alive_for_raii;
 
         if( text.front() != '[' )
             throw std::runtime_error( "Parse error, expected [" );
@@ -41,7 +47,7 @@ struct thing
             case '[':
             {
                 if( constructor_output )
-                    std::cout << std::string( indent, ' ' ) << "nom [\n";
+                    dbg_indented() << "nom [\n";
                 thing subthing( text.substr( text_consumed ) );
                 text_consumed += subthing.text_consumed;
                 m_things.push_back( subthing );
@@ -49,18 +55,18 @@ struct thing
             break;
             case ']':
                 if( constructor_output )
-                    std::cout << std::string( indent, ' ' ) << "nom ]\n";
+                    dbg_indented() << "nom ]\n";
                 ++text_consumed;
                 return;
             case ',':
                 if( constructor_output )
-                    std::cout << std::string( indent, ' ' ) << "nom ,\n";
+                    dbg_indented() << "nom ,\n";
                 ++text_consumed;
                 break;
             default:
             {
                 if( constructor_output )
-                    std::cout << std::string( indent, ' ' ) << "nom an int\n";
+                    dbg_indented() << "nom an int\n";
                 std::size_t consumed_for_int;
                 m_things.push_back( std::stoi( text.substr( text_consumed ), &consumed_for_int ) );
                 text_consumed += consumed_for_int;
@@ -74,51 +80,54 @@ struct thing
     std::size_t text_consumed{};
     std::vector< std::variant< thing, int > > m_things;
 
-    friend bool operator<=( int const l, thing const& r )
+    friend std::strong_ordering operator<=>( int const l, thing const& r )
     {
-        if( r.m_things.empty() )
-            return 1;
-
-        return std::visit( [ & ]( auto const& r_item ) { return l <= r_item; }, r.m_things.front() );
+        return thing( "[" + std::to_string( l ) + "]" ) <=> r;
     }
-    friend bool operator<=( thing const& l, int const r )
+    friend std::strong_ordering operator<=>( thing const& l, int const r )
     {
-        if( l.m_things.empty() )
-            return true;
-        return std::visit( [ & ]( auto const& l_item ) { return l_item <= r; }, l.m_things.front() );
+        return l <=> thing( "[" + std::to_string( r ) + "]" );
     }
-    friend bool operator<=( thing const& l, thing const& r )
+    friend std::strong_ordering operator<=>( thing const& l, thing const& r )
     {
         std::cout << "Checking " << l << " vs " << r << '\n';
         for( std::size_t i{}; i < l.m_things.size(); ++i )
         {
             if( r.m_things.size() <= i )
             {
-                std::cout << "false\n";
-                return false;
+                std::cout << "false bc right ran out of stuff\n";
+                return std::strong_ordering::greater;
             }
-            if( !std::visit( []( auto const& l_item, auto const& r_item ) {
-                             std::cout << "Compare " << l_item << " and " << r_item << "...";
-                             if(l_item <= r_item) std::cout << "true\n";
-                             else std::cout << "false\n";
-                    return l_item <= r_item;
+            if( auto const itemwise_compare = std::visit( []( auto const& l_item, auto const& r_item ) {
+                    std::cout << "Compare " << l_item << " and " << r_item << "...";
+                    auto output_value{ l_item <=> r_item };
+                    if( output_value < 0 )
+                    {
+                        std::cout << "Less\n";
+                    }
+                    else if( output_value > 0 )
+                    {
+                        std::cout << "Greater\n";
+                    }
+                    else
+                        std::cout << "Equal\n";
+                    return output_value;
                 },
-                             l.m_things.at( i ), r.m_things.at( i ) ) )
+                                                          l.m_things.at( i ), r.m_things.at( i ) );
+                itemwise_compare != 0 )
             {
-                std::cout << "false\n";
-                return false;
+                return itemwise_compare;
             }
         }
-                std::cout << "true\n";
-        return true;
+        return l.m_things.size() <=> r.m_things.size();
     }
 };
 
-std::ostream& operator<<(std::ostream& os, thing const& t)
+std::ostream& operator<<( std::ostream& os, thing const& t )
 {
     os << '[';
-    for(auto const& whatever : t.m_things)
-    std::visit([&](auto const& item){os << item << ',';},whatever);
+    for( auto const& whatever : t.m_things )
+        std::visit( [ & ]( auto const& item ) { os << item << ','; }, whatever );
     os << ']';
 
     return os;
@@ -131,10 +140,12 @@ advent_t advent( std::vector< std::string > const& input )
     {
         std::cout << input[ i ] << '\n'
                   << input[ i + 1 ] << '\n';
-        thing a(input[i]);
-        thing b(input[i+1]);
-        std::cout << "Parsed:\n" << a << '\n' << b << '\n';
-        if(a <= b)
+        thing a( input[ i ] );
+        thing b( input[ i + 1 ] );
+        std::cout << "Parsed:\n"
+                  << a << '\n'
+                  << b << '\n';
+        if( a <= b )
         {
             std::cout << "Pair " << i / 3 << " Good. Less than\n\n";
             sum += 1 + i / 3;

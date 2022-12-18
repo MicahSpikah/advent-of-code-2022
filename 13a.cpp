6360dc6cc6ef84365a5c1986e3c96bde08eaea6e
Micah TerHaar <micah.terhaar@gentex.com>
2022-12-17 22:17:56 -0500

    Day 13

--- 13a.cpp
+++ 13a.cpp
@@ -28,13 +28,6 @@ struct thing
 {
     thing( std::string const& text )
     {
-        bool constructor_output{};
-
-        if( constructor_output )
-            dbg_indented() << "Parsing: "s << text << '\n';
-
-        //indent_up name_this_to_keep_alive_for_raii;
-
         if( text.front() != '[' )
             throw std::runtime_error( "Parse error, expected [" );
 
@@ -46,27 +39,19 @@ struct thing
             {
             case '[':
             {
-                if( constructor_output )
-                    dbg_indented() << "nom [\n";
                 thing subthing( text.substr( text_consumed ) );
                 text_consumed += subthing.text_consumed;
                 m_things.push_back( subthing );
             }
             break;
             case ']':
-                if( constructor_output )
-                    dbg_indented() << "nom ]\n";
                 ++text_consumed;
                 return;
             case ',':
-                if( constructor_output )
-                    dbg_indented() << "nom ,\n";
                 ++text_consumed;
                 break;
             default:
             {
-                if( constructor_output )
-                    dbg_indented() << "nom an int\n";
                 std::size_t consumed_for_int;
                 m_things.push_back( std::stoi( text.substr( text_consumed ), &consumed_for_int ) );
                 text_consumed += consumed_for_int;
@@ -90,31 +75,13 @@ struct thing
     }
     friend std::strong_ordering operator<=>( thing const& l, thing const& r )
     {
-        std::cout << "Checking " << l << " vs " << r << '\n';
         for( std::size_t i{}; i < l.m_things.size(); ++i )
         {
             if( r.m_things.size() <= i )
             {
-                std::cout << "false bc right ran out of stuff\n";
                 return std::strong_ordering::greater;
             }
-            if( auto const itemwise_compare = std::visit( []( auto const& l_item, auto const& r_item ) {
-                    std::cout << "Compare " << l_item << " and " << r_item << "...";
-                    auto output_value{ l_item <=> r_item };
-                    if( output_value < 0 )
-                    {
-                        std::cout << "Less\n";
-                    }
-                    else if( output_value > 0 )
-                    {
-                        std::cout << "Greater\n";
-                    }
-                    else
-                        std::cout << "Equal\n";
-                    return output_value;
-                },
-                                                          l.m_things.at( i ), r.m_things.at( i ) );
-                itemwise_compare != 0 )
+            if( auto const itemwise_compare = std::visit( []( auto const& l_item, auto const& r_item ) { return l_item <=> r_item; }, l.m_things.at( i ), r.m_things.at( i ) ); itemwise_compare != 0 )
             {
                 return itemwise_compare;
             }

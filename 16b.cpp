f6bea5dfddee1e2f740ffc54eb1160a23f5df6c2
Micah TerHaar <micah.terhaar@gentex.com>
2023-01-30 23:48:18 -0500

    This gets the right answer on test data. Taking a while to run on input but I'm confident it'll get there

--- 16b.cpp
+++ 16b.cpp
@@ -10,26 +10,52 @@ struct state
 {
     std::map< std::string, std::map< std::string, int > > distance;
     int flow{};
-    int minutes_remaining{ 30 };
-    std::string loc{ "AA" };
+    int t1{ 26 };
+    int t2{ 26 };
+    std::string l1{ "AA" };
+    std::string l2{ "AA" };
 };
 
 int best_from( state s )
 {
-    int best = s.flow;
+    int best         = s.flow;
+    state best_state = s;
 
-    for( auto const& next : s.distance.at( s.loc ) )
-        if( next.second <= s.minutes_remaining )
+    for( auto const& next : s.distance.at( s.l2 ) )
+    {
+        if( next.second <= s.t2 )
         {
             state mod = s;
-            mod.minutes_remaining -= next.second;
-            mod.flow += mod.minutes_remaining * flow_rate.at( next.first );
-            mod.distance.erase( mod.loc );
+            mod.t2 -= next.second;
+            mod.flow += mod.t2 * flow_rate.at( next.first );
+            mod.l2 = next.first;
             for( auto& room : mod.distance )
-                room.second.erase( mod.loc );
-            mod.loc = next.first;
-            best    = std::max( best_from( mod ), best );
+                room.second.erase( mod.l2 );
+            best = std::max( best, best_from( mod ) );
         }
+    }
+    if( best != s.flow )
+    {
+        auto mod = s;
+        mod.t2   = 0;
+        best     = std::max( best, best_from( mod ) );
+    }
+    else
+    {
+        for( auto const& next : s.distance.at( s.l1 ) )
+        {
+            if( next.second <= s.t1 )
+            {
+                state mod = s;
+                mod.t1 -= next.second;
+                mod.flow += mod.t1 * flow_rate.at( next.first );
+                mod.l1 = next.first;
+                for( auto& room : mod.distance )
+                    room.second.erase( mod.l1 );
+                best = std::max( best, best_from( mod ) );
+            }
+        }
+    }
 
     return best;
 }
@@ -79,4 +105,28 @@ advent_t advent( std::vector< std::string > const& input )
     }
 
     return best_from( initial );
+    /*
+        while( true )
+    {
+        auto out = best_from( initial );
+        if( out.second.t1 == initial.t1 && out.second.t2 == initial.t2 )
+        {
+            return out.first;
+        }
+        else if(out.second.t1 == initial.t1)
+        {
+            std::cerr << "Minute " << ( 27 - out.second.t2) << ": ELEPHANT Move to " << out.second.l2<< " and open, flow at " << out.second.flow << "\n";
+            initial = out.second;
+        }
+        else
+        {
+            std::cerr << "Minute " << ( 27 - out.second.t1) << ": Move to " << out.second.l1<< " and open, flow at " << out.second.flow << "\n";
+            initial = out.second;
+        }
+        std::cerr << "Places you can get from AA:";
+        for(auto const dst : out.second.distance.at("AA"))
+            std::cerr << ' ' << dst.first;
+        std::cerr << '\n';
+    }
+    */
 }
